15b24b5fb2bd1e235a448308f75793887f4ebe08
Abdo El Hage <47995042+a-hage@users.noreply.github.com>
2026-06-17 10:58:54 +0200

    Refactor blockchain index handling and filtering

--- subrion-blockchain/index.php
+++ subrion-blockchain/index.php
@@ -1,40 +1,56 @@
 <?php
-	#$iaBlockchain = $iaCore->factoryModule('blockchain', IA_CURRENT_MODULE);
 
-	#$baseUrl = $iaCore->factory('page', iaCore::FRONT)->getUrlByName('blockchain');
-	
-	if (iaView::REQUEST_HTML == $iaView->getRequestType())
-	{
-		if(isset($_POST['category_filter'])){
-			$categoryFilter = $_POST['category_filter']; 
-			$blockchainFilter = $iaCore->factoryItem('blockchain')->getBlockchainFilter($categoryFilter);
-			$iaView->assign('categoryFilter',$categoryFilter);
-			$iaView->assign('blockchainFilter',$blockchainFilter);
-		}else{
-			$categoryFilter = "";
-			$blockchainFilter = $iaCore->factoryItem('blockchain')->get();
-			$iaView->assign('categoryFilter',$categoryFilter);
-			$iaView->assign('blockchainFilter',$blockchainFilter);
-		}
-			
-		$items = $iaCore->factoryItem('blockchain')->get();
-		$usedCategory = $iaCore->factoryItem('blockchain')->getUsedCategory();
-		$blocks = array();
-		foreach($items as $item){
-			extract($item);
-			$item['category']= $iaCore->factoryItem('blockchain')->getBlockchainCategory($item['name']);
-			array_push($blocks,$item);
-		}
-			
-		$iaView->assign('usedCategory',$usedCategory);
-		
-		$iaView->assign('items',$items);
-		$iaView->assign('blocks',$blocks);
-		//print_r($blocks);
-			
-		$iaView->display('index');	
+$iaBlockchain = $iaCore->factoryModule('blockchain', IA_CURRENT_MODULE);
 
-	}
-?>
+// Seiten-URL holen (optional)
+$baseUrl = $iaCore->factory('page', iaCore::FRONT)->getUrlByName('blockchain');
+$comparePageUrl = $iaCore->factory('page', iaCore::FRONT)->getUrlByName('blockchain_compare');
+$detailPageUrl = $iaCore->factory('page', iaCore::FRONT)->getUrlByName('blockchain_detail');
 
+// Nur bei HTML Requests ausführen
+if (iaView::REQUEST_HTML == $iaView->getRequestType())
+{
+    // Kategorie-Filter vorhanden
+    if (isset($_POST['category_filter']) && !empty($_POST['category_filter'])) {
 
+        $categoryFilter = $_POST['category_filter'];
+
+        // korrekt: kein factoryItem mehr
+        $blockchainFilter = $iaBlockchain->getBlockchainFilter($categoryFilter);
+
+        $iaView->assign('categoryFilter', $categoryFilter);
+        $iaView->assign('blockchainFilter', $blockchainFilter);
+
+    } else {
+
+        $categoryFilter = [];
+
+        // alle Daten holen
+        $blockchainFilter = $iaBlockchain->get();
+
+        $iaView->assign('categoryFilter', $categoryFilter);
+        $iaView->assign('blockchainFilter', $blockchainFilter);
+    }
+
+    // Basisdaten laden
+    $items = $iaBlockchain->get();
+    $usedCategory = $iaBlockchain->getUsedCategory();
+
+    $blocks = [];
+
+    foreach ($items as $item) {
+        // Kategorien sauber setzen
+        $item['category'] = $iaBlockchain->getBlockchainCategory($item['name']);
+        $blocks[] = $item;
+    }
+
+    // an Template übergeben
+    $iaView->assign('usedCategory', $usedCategory);
+    $iaView->assign('items', $items);
+    $iaView->assign('blocks', $blocks);
+    $iaView->assign('comparePageUrl', $comparePageUrl);
+    $iaView->assign('detailPageUrl', $detailPageUrl);
+
+    // Template rendern
+    $iaView->display('index');
+}
