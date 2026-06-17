45ae6002fe568db05c1d29036b8ea8ea19328bcf
Abdo El Hage <47995042+a-hage@users.noreply.github.com>
2026-06-17 10:57:33 +0200

    Refactor SQL query and update table prefixes

--- subrion-blockchain/categoryfilter.php
+++ subrion-blockchain/categoryfilter.php
@@ -1,20 +1,22 @@
 <?php
 	if (iaView::REQUEST_HTML == $iaView->getRequestType())
-	{ 
+	{
 		$sql=<<<SQL
-SELECT b_c.`categoryId`,b.`website`,bc.`name` FROM `:table_blockchain_category` b_c INNER JOIN `:table_blockchaincategory` bc ON (b_c.`categoryId` = bc.`id`)  
+SELECT b_c.`categoryId`, b.`website`, bc.`name`
+FROM `:table_blockchain_category` b_c
+INNER JOIN `:table_blockchaincategory` bc ON (b_c.`categoryId` = bc.`id`)
+INNER JOIN `:table_blockchain` b ON (b_c.`blockchainId` = b.`id`)
 SQL;
 
- $sql = $iaDb::printf($sql, [
-					'prefix' => $iaDb->prefix,
-					'table_blockchaincategory' => iaBlockchainCategory::getTable(true),
-					'table_blockchain_category' => iaBlockchain_Category::getTable(true)
-				]);
-				
+		$sql = iaDb::printf($sql, [
+			'prefix' => $iaDb->prefix,
+			'table_blockchain' => $iaDb->prefix . 'blockchain',
+			'table_blockchaincategory' => $iaDb->prefix . 'blockchaincategory',
+			'table_blockchain_category' => $iaDb->prefix . 'blockchain_category'
+		]);
 		$usedCategory = $iaDb->getAll($sql);
 		//print_r($usedCategory);
 	}
-	//echo "UsedCategory";
 	$iaView->assign('usedCategory',  $usedCategory);
-	$iaView->display('categoryfilter');	
+	$iaView->display('categoryfilter');
 ?>
